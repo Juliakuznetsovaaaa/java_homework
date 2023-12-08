4a8af87bcbad1dbedb10fe3b746f2f3e125db431
Juliakuznetsovaaaa <jyliakuz@gmail.com>
2023-12-08 19:30:57 +0300

    all homework done

---
+++ _06/_8_reflection.java
@@ -0,0 +1,36 @@
+package _06;
+import _02._1_String;
+import _04._7_classes;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+public class _8_reflection {
+    public static void printClassInfo(Class<?> clazz) {
+        String className = clazz.getName();
+        System.out.println("Class name: " + className);
+
+        System.out.println("Fields:");
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            int modifiers = field.getModifiers();
+            System.out.println(Modifier.toString(modifiers) + " " + field.getType().getName() + " " + field.getName());
+        }
+
+        System.out.println("Methods:");
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            int modifiers = method.getModifiers();
+            String methodName = method.getName();
+            String returnType = method.getReturnType().getName();
+            String parameters = Arrays.toString(method.getParameterTypes());
+            System.out.println(Modifier.toString(modifiers) + " " + returnType + " " + methodName + parameters);
+        }
+    }
+
+    public static void main(String[] args) {
+        printClassInfo(String.class); //Вывод информации о классе String
+    }
+}
+
---
+++ _06/_9.java
@@ -0,0 +1,25 @@
+package _06;
+import java.lang.reflect.Method;
+
+class MyClass {
+    public void myMethod() {
+        System.out.println("Метод myMethod() был вызван");
+    }
+}
+
+public class _9 {
+    public static void main(String[] args) throws Exception {
+        //Получаем объект Class по имени класса
+        Class<?> clazz = Class.forName("_06.MyClass");
+        //Создаем экземпляр класса с использованием конструктора по умолчанию
+        Object instance = clazz.getDeclaredConstructor().newInstance();
+        String methodName = "myMethod";
+        //Получаем объект Method по имени метода
+        Method method = clazz.getDeclaredMethod(methodName);
+        //Вызываем метод
+        method.invoke(instance);
+        //Вывод:Метод myMethod() был вызван
+    }
+}
+
+
